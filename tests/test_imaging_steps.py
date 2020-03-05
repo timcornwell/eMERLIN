0b459de9d4377622814874668c3c9067cd641626
Tim Cornwell <realtimcornwell@gmail.com>
2020-03-05 13:14:31 +0000

    Set path for json test file

--- tests/test_imaging_steps.py
+++ tests/test_imaging_steps.py
@@ -6,6 +6,8 @@ import logging
 import unittest
 
 from erp.functions.pipelines import run_erp
+from erp.functions.erp_path import erp_path
+from erp.drivers.driver_support import get_logger
 
 log = logging.getLogger('logger')
 log.setLevel(logging.WARNING)
@@ -16,11 +18,11 @@ class TestERP_functions(unittest.TestCase):
         pass
     
     def test_pipeline(self):
-        defaults_file = 'test_params.json'
+        defaults_file = erp_path('tests/test_params.json')
         log.info('Loading default parameters from {0}:'.format(defaults_file))
         with open(defaults_file) as df:
             json_params = json.loads(df.read())
-        run_erp(json_params)
+        run_erp(json_params, get_logger=get_logger)
 
 if __name__ == '__main__':
     unittest.main()
