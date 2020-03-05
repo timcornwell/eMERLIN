b03b0317f8f42d58e7dec6bf319c1facbc8bad67
Tim Cornwell <realtimcornwell@gmail.com>
2020-03-05 16:54:44 +0000

    Stage channels data

--- examples/pipelines/galahad/run_pipeline.sh
+++ examples/pipelines/galahad/run_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pwd
 export PYTHONPATH=/home/cornwell/Code/eMERLIN_RASCIL_pipeline:/home/cornwell/Code/rascil
-python3 erp/drivers/erp_script.py --params eMERLIN_3C277_1.json
+python3 /home/cornwell/Code/eMERLIN_RASCIL_pipeline/erp/drivers/erp_script.py --params eMERLIN_3C277_1.json
